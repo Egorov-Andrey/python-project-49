7656192a2e9bdb6685fa074bb2f2b74f8c40398f
Andrey Egorov <andreye89@mail.ru>
2025-08-27 18:00:54 +0300

    created "consts.py"

---
+++ brain_games/consts.py
@@ -0,0 +1,8 @@
+EVEN_INSTUCTION = 'Answer "yes" if the number is even, answer "no".'
+CALC_INSTUCTION = 'What is the result of the expresion?'
+GCD_INSTUCTION = 'Find the greatest common divisor of given numbers.'
+PROGRSSION_INSTUCTION = 'What number is missing in the progression?'
+PRIME_INSTUCTION = 'Answer "yes" if given number is prime.' \
+                    ' Otherwise answer"no".'
+MATH_SIGNS = ('+', '-', '*')
+AMOUNT_OF_ROUND = 3
