5c9b768e5fe3a4f16af5cb6856720d3af10b3f75
Andrey Egorov <andreye89@mail.ru>
2025-08-27 17:29:28 +0300

    created fun"run_even_game"

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,50 +1,12 @@
-from random import randint
-
-import prompt
+#!/usr/bin/env python3
+from brain_games.games.even import run_even_game
 
 
 def main():
-
-    number_question = randint(1, 100)
-
-    print('Welcome to the Brain Games!')
-    name = prompt.string('May I have your name? ')
-
-    print('Hello, ' + name + '!')
-
-    print('Answer "yes" if the number is even, otherwise answer "no".')
-
-    num_correct_input = 0
-    num_of_times = 2
-    number_question = randint(1, 100)
-    print('Question: ' + str(number_question))
-    answer_user = prompt.string('Your answer: ')
-    if answer_user == even_or_otherwise(number_question):
-        print('Correct!')
-
-    while num_correct_input < num_of_times:
-
-        if answer_user == even_or_otherwise(number_question):
-            number_question = randint(1, 100)
-            print('Question: ' + str(number_question))
-            answer_user = prompt.string('Your answer: ')
-            print('Correct!')
-            num_correct_input += 1
-            if num_correct_input == 3:
-                print('Congratulations, ' + name + '!')
-        elif answer_user != even_or_otherwise(number_question):
-            print("'yes' is wrong answer ;(. Correct answer was 'no'.")
-            print("Let's try again, " + name + '!')
-            break
-        else:
-            print("'yes' is wrong answer ;(. Correct answer was 'no'.")
-            print("Let's try again, " + name + '!')
-            break
-          
-    print('Congratulations, ' + name + '!')
+    run_even_game()
 
 
-def even_or_otherwise(number_question):
-    if number_question % 2 == 0:
-        return 'yes'
-    return 'no'
+if __name__ == '__main__':
+    main()
+
+
