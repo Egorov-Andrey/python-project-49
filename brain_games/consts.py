8c2c3793536cd43d085a488e099da426bc1c0f0f
Andrey Egorov <andreye89@mail.ru>
2025-09-02 11:04:22 +0300

    update brain_games and dist

--- brain_games/consts.py
+++ brain_games/consts.py
@@ -1,4 +1,4 @@
-EVEN_INSTUCTION = 'Answer "yes" if the number is even, answer "no".'
+EVEN_INSTUCTION = 'Answer "yes" if the number is even, otherwise answer "no".'
 CALC_INSTUCTION = 'What is the result of the expresion?'
 GCD_INSTUCTION = 'Find the greatest common divisor of given numbers.'
 PROGRSSION_INSTUCTION = 'What number is missing in the progression?'
