e4c0e145b2635ceaa86f2c2851d229f71e1df32c
Andrey Egorov <andreye89@mail.ru>
2025-09-02 15:19:28 +0300

    update consts.py

--- brain_games/consts.py
+++ brain_games/consts.py
@@ -1,5 +1,5 @@
 EVEN_INSTUCTION = 'Answer "yes" if the number is even, otherwise answer "no".'
-CALC_INSTUCTION = 'What is the result of the expresion?'
+CALC_INSTUCTION = 'What is the result of the expression?'
 GCD_INSTUCTION = 'Find the greatest common divisor of given numbers.'
 PROGRSSION_INSTUCTION = 'What number is missing in the progression?'
 PRIME_INSTUCTION = 'Answer "yes" if given number is prime.' \
