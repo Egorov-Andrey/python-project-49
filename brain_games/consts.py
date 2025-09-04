465f7f2b8437f96af4f7f0c07b9526ed101e8859
Andrey <60265614+Egorov-Andrey@users.noreply.github.com>
2025-09-04 21:19:29 +0300

    Delete brain_games/consts.py

--- brain_games/consts.py
+++
@@ -1,9 +0,0 @@
-EVEN_INSTUCTION = 'Answer "yes" if the number is even, otherwise answer "no".'
-CALC_INSTUCTION = 'What is the result of the expression?'
-GCD_INSTUCTION = 'Find the greatest common divisor of given numbers.'
-PROGRSSION_INSTUCTION = 'What number is missing in the progression?'
-PRIME_INSTUCTION = 'Answer "yes" if given number is prime.' \
-                    ' Otherwise answer"no".'
-MATH_SIGNS = ('+', '-', '*')
-AMOUNT_OF_ROUND = 3
-MIN_PROGR_LENGHT, MAX_PROGR_LENGHT = 5, 10
